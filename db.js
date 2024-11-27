935b6c9d90b9baf2c59737d46e6ab60f8fc7b3b0
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-11-27 14:55:06 +0000

    create user function

--- db.js
+++ db.js
@@ -1,1 +1,33 @@
 const sqlite3 = require('sqlite3').verbose();
+
+// Create a new database file if it doesn't exist
+const db = new sqlite3.Database('./dinocab.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+});
+
+function setupDB() {
+    // Create users table
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    ipAddress TEXT NOT NULL,
+    savings INTEGER DEFAULT 0,
+    rides INTEGER DEFAULT 0,)`);
+}
+
+setupDB();
+
+function createUser(username, ipAddress) {
+    return new Promise((resolve, reject) => {
+        db.run(`INSERT INTO users (username, ipAddress) VALUES (?, ?)`, [username, ipAddress], function (err) {
+            if (err) {
+                reject(err);
+            }
+            resolve(this.lastID);
+        });
+    });
+}
+
+module.exports = { db, createUser };
